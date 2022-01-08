3b19e7c0b463ea9584edb2d7ad5cbd9e2773275e
WeiHung <wei_hung.chin@daimler.com>
2022-01-08 15:41:40 +0800

    Init

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserModule,
+  ]
+})
+export class CoreModule { }
